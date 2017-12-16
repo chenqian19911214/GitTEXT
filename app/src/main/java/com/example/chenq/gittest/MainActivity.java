76627ed2543085ed87129835b4d7fee95219818a
chenqian1991 <1096011884@qq.com>
2017-12-17 00:57:51 +0800

    2017年12月17日00:57:46

--- app/src/main/java/com/example/chenq/gittest/MainActivity.java
+++ app/src/main/java/com/example/chenq/gittest/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.chenq.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * chenqian 2017年12月17日00:57:23
+ * */
 public class MainActivity extends AppCompatActivity {
 
     @Override
